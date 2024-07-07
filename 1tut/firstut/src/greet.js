5c176dc0f68adfff9a77b6a73f8519dd6adfa1c8
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-07-07 15:21:20 +0530

    Functional and state Components

---
+++ 1tut/firstut/src/greet.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+//functional components  recieves props and renders html
+// function Greet(){
+//     return <h1>Hello Rushikesh!!!</h1>
+// }
+
+//named export
+export const Greet = () => <h1>Hello Rushikesh!!</h1> 
+//while importing ==>> import {Greet} from './greet'
+
+
+//default export
+// export default Greet 
+//while importing ==>> import anyname from'./greet'
