e06506cb3d9b395b77b7cbba52cb095a3678f3c7
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-07-11 19:20:08 +0530

    Lists and Keys

--- 2tut/src/components/NameList.js
+++ 2tut/src/components/NameList.js
@@ -5,15 +5,21 @@ function NameList() {
 
   const person = [
     { id: 1, name: "Rushiii", age: 19, skill: "nothing" },
-    { id: 1, name: "Rushiii", age: 19, skill: "nothing" },
-    { id: 1, name: "Rushiii", age: 19, skill: "nothing" },
-    { id: 1, name: "Rushiii", age: 19, skill: "nothing" },
+    { id: 2, name: "Rushiii", age: 19, skill: "nothing" },
+    { id: 3, name: "Rushiii", age: 19, skill: "nothing" },
+    { id: 4, name: "Rushiii", age: 19, skill: "nothing" },
   ];
 
   const renderedarr = array.map((name) => <h1>{name}</h1>);
 
-  const personlist = person.map((person) => <Person person={person}/>) //good practice
   // const personlist = person.map((person) => <h1>My name is {person.name} , id = {person.id} , age={person.age}, skill={person.skill}</h1>)
+
+  //we need to pass key prop which is unique within list for each item,  this key prop is not accesible in child component
+  //this key helps react to handle UI modifications nicely
+  //a key is a special string attribute we need to include when creating list of elements
+  //keys gives the elements a stable identity
+  //efficient update of UI
+  const personlist = person.map((person) => <Person key={person.id} person={person}/>) //good practice
   return (
     <div>
 
