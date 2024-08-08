54fdb6e67d6da6094e8a8dc59bb6166c9f9010b5
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-08 20:14:17 +0530

    Passing key as attribute in fragments

--- 3tut/src/components/Column.js
+++ 3tut/src/components/Column.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 function Column() {
     /*td cannot have div as its parent element therefore we use react fragments */
+
+    //only key can be passed as attribute to the fragment
+
+    const item = [];
+
+
   return (  
+    // <> </>   this is also react fragment only
     <React.Fragment>   
+        {
+            item.map(item => (
+                <React.Fragment key={item.id}>
+                    <h1>Title</h1>
+                    <p>{item.title}</p>
+                </React.Fragment>
+            ))
+        }
         <td>Name : </td>
         <td>Rushikesh</td>
     </React.Fragment>
